c6a05dd871968516ecb2f850c06d352576fb90f8
adhit7 <adhithyanalan@gmail.com>
2025-05-08 13:59:39 +0530

    deployment changes

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
   base: '',
+  build: {
+    outDir: 'dist',
+    assetsInlineLimit: 4096,
+    rollupOptions: {
+      input: 'index.html',
+    },
+  },
   plugins: [react(), tailwindcss()],
 });
